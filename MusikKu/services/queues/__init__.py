6b8cd3e65658b596060d4b6f782e8db9f7937658
Stevan KZ <87715535+SatrugnaFD@users.noreply.github.com>
2021-07-22 22:40:10 +0700

    Create __init__.py

---
+++ MusikKu/services/queues/__init__.py
@@ -0,0 +1,7 @@
+from MusicKu.services.queues.queues import clear 
+from MusicKu.services.queues.queues import get
+from MusicKu.services.queues.queues import is_empty
+from MusicKu.services.queues.queues import put
+from MusicKu.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
