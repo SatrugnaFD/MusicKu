f867713810864d405cb2cbe2ed56ca6756eb1785
Stevan KZ <87715535+SatrugnaFD@users.noreply.github.com>
2021-07-23 12:20:54 +0700

    Update __init__.py

--- MusikKu/services/converter/__init__.py
+++ MusikKu/services/converter/__init__.py
@@ -4,6 +4,6 @@ from os import mkdir
 if 'raw_files' not in listdir():
     mkdir('raw_files')
 
-from MusicMan.services.converter.converter import convert
+from MusicKz.services.converter.converter import convert
 
 __all__ = ["convert"]
